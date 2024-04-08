308fc5fe45371d3b88e79e72fb38111cbf976d08
KhairulAmirinKA <u2101603@siswa.um.edu.my>
2024-04-08 17:18:38 +0800

    UI for Main Activity, EnterItemDetails

--- app/src/main/java/com/khairul/pricecomparator/EnterItemDetails.java
+++ app/src/main/java/com/khairul/pricecomparator/EnterItemDetails.java
@@ -1,27 +1,26 @@
 package com.khairul.pricecomparator;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class EnterItemDetails extends AppCompatActivity {
 
-    ConstraintLayout CLNewRecord;
+    ImageView IVBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_enter_item_details);
 
-        CLNewRecord = findViewById(R.id.CLNewRecord);
-
-        CLNewRecord.setOnClickListener(new View.OnClickListener() {
+        IVBack = findViewById(R.id.IVBack);
+        IVBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent  = new Intent(MainActivity.this, EnterItemDetails.class);
+                Intent intent  = new Intent(EnterItemDetails.this, MainActivity.class);
                 startActivity(intent);
             }
         });

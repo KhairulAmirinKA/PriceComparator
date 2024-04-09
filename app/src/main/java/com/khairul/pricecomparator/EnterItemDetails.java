bf3dfc7180940492d8a63712a69b52a37e0844b6
KhairulAmirinKA <u2101603@siswa.um.edu.my>
2024-04-09 10:58:52 +0800

    complete UI for EnterItemDetails

--- app/src/main/java/com/khairul/pricecomparator/EnterItemDetails.java
+++ app/src/main/java/com/khairul/pricecomparator/EnterItemDetails.java
@@ -5,18 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 public class EnterItemDetails extends AppCompatActivity {
 
     ImageView IVBack;
+    EditText ETProductName, ETProductPrice, ETProductQuantity;
+
+    Button BtnAddDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_item_details);
 
-        IVBack = findViewById(R.id.IVBack);
+
         IVBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -24,5 +29,15 @@ public class EnterItemDetails extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void initViews(){
+        IVBack = findViewById(R.id.IVBack);
+
+        ETProductName = findViewById(R.id.ETProductName);
+        ETProductPrice = findViewById(R.id.ETProductPrice);
+        ETProductQuantity = findViewById(R.id.ETProductQuantity);
+
+        BtnAddDetails = findViewById(R.id.BtnAddDetails);
     }
 }
